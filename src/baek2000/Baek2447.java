9e5683f5caf0e3ca7ab201a5bfaca5815a613ae0
JogiYo <jo930921@gmail.com>
2019-06-28 16:40:03 +0900

    BOJ2447, Identify patterns and use recursion(point star)

--- src/baek2000/Baek2447.java
+++ src/baek2000/Baek2447.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+// BOJ2447, Identify patterns and use recursion(point star)
 public class Baek2447 {
 	private static char[][] starMap;
 	public static void main(String[] args) throws Exception{
