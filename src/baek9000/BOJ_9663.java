3c84e150206b4c66537c812920942e646a868e86
JogiYo <jo930921@gmail.com>
2021-03-10 15:59:41 +0900

    BOJ 9663 : N Queen using backtracking, recursion Math.abs(depth-i) == Math.abs(col[depth] - col[i]) The process of checking if it exists on the diagonal. I think it's good to know

---
+++ src/baek9000/BOJ_9663.java
@@ -0,0 +1,56 @@
+package baek9000;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// BOJ 9663 : N Queen
+// using backtracking, recursion
+// Math.abs(depth-i) == Math.abs(col[depth] - col[i]) The process of checking if it exists on the diagonal. I think it's good to know
+public class BOJ_9663 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+	
+		int n = Integer.parseInt(br.readLine());
+		
+		sb.append(solution(n) + "");
+		System.out.print(sb);
+	}
+	
+	static int answer = 0;
+	static int x = 0;
+	static int col[];
+	
+	public static int solution(int n) {
+		// One queen can come in a row.
+		// The col value ranges from 0 to n-1, and these values ​​indicate the position.
+        col = new int[n];
+        x = n;
+        queen(0); // Start with the first row.
+        return answer;
+    }
+	
+	private static void queen(int depth) {
+		if(depth == x) { // If you filled in the values ​​for each row, the condition was satisfied.
+			answer++;
+		}
+		else {
+			for(int i = 0; i < x; ++i) {
+				col[depth] = i;
+				if(isPossible(depth)) { // Checks by putting a value in the row.
+					// If satisfied, proceed to next row
+					queen(depth+1);
+				}
+			}
+		}
+	}
+	
+	private static boolean isPossible(int depth) {
+		for(int i = 0; i < depth; ++i) {
+			if(col[i] == col[depth]) return false; // When the queen is in a straight line
+			if(Math.abs(depth-i) == Math.abs(col[depth] - col[i])) return false; // ** When the queen is on the diagonal **
+		}
+		
+		return true;
+	}
+}
