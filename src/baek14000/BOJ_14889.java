767b23da5e8668adb4c511a377d08fca577cc750
JogiYo <jo930921@gmail.com>
2021-06-02 14:50:21 +0900

    BOJ 14889 : Start and Link Backtracking, using Combination

--- src/baek14000/BOJ_14889.java
+++ src/baek14000/BOJ_14889.java
@@ -6,13 +6,15 @@ import java.io.InputStreamReader;
 // BOJ 14889 : Start and Link
 // Backtracking, using Combination
 public class BOJ_14889 {
-	static StringBuilder sb = new StringBuilder();
 	static int MIN = Integer.MAX_VALUE;
+	static int n;
+	static int[][] s;
+	static boolean[] visit;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int n = Integer.parseInt(br.readLine());
-		int[][] s = new int[n][n];
+		n = Integer.parseInt(br.readLine());
+		s = new int[n][n];
 		for(int i = 0; i < n; ++i) {
 			String[] input = br.readLine().split(" ");
 			for(int j = 0; j < n; ++j) {
@@ -20,26 +22,36 @@ public class BOJ_14889 {
 			}
 		}
 		
-		boolean[] visit = new boolean[n];
-		comb(s, visit, 0, n, 0);
-		sb.append(MIN + "");
-		System.out.print(sb);
+		visit = new boolean[n];
+		comb(0, 0);
+		System.out.print(MIN);
 	}
 	// nCr, start is start index
-	private static void comb(int[][] s, boolean[] visit, int start, int n, int r) {
-		if(r == (n/2)) {
-			calc(s, visit, n);
+	private static void comb(int start, int r) {
+		if(r == n/2) {
+			int differ = calc();
+			// If the score difference between the two teams is 0, 
+			// it is the lowest minimum value,
+			// so there is no need to search further and output 0 and exit.
+			if(differ == 0) {
+				System.out.println(differ);
+				System.exit(0);
+			}
+			
+			MIN = Math.min(MIN, differ);
 			return;
 		}
 		// combination
 		for(int i = start; i < n; ++i) {
-			visit[i] = true;
-			comb(s, visit, start + 1, n, r + 1);
-			visit[i] = false;
+			if(!visit[i]) {
+				visit[i] = true;
+				comb(i + 1, r + 1);
+				visit[i] = false;
+			}
 		}
 	}
 
-	private static void calc(int[][] s, boolean[] visit, int n) {
+	private static int calc() {
 		int t_start = 0;
 		int t_link = 0;
 		
@@ -49,7 +61,7 @@ public class BOJ_14889 {
 					t_start += s[i][j];
 					t_start += s[j][i];
 				}
-				else if((!visit[i])&&(!visit[j])) {
+				else if((!visit[i]) && (!visit[j])) {
 					t_link += s[i][j];
 					t_link += s[j][i];
 				}
@@ -57,14 +69,6 @@ public class BOJ_14889 {
 		}
 		
 		int differ = Math.abs(t_start - t_link);
-		// If the score difference between the two teams is 0, 
-		// it is the lowest minimum value,
-		// so there is no need to search further and output 0 and exit.
-		if(differ == 0) {
-			System.out.println(differ);
-			System.exit(0);
-		}
-		
-		MIN = Math.min(MIN, differ);
+		return differ;
 	}
 }
