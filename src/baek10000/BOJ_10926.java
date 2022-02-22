4885f559bfab6e692c9068afc4bd12c092f26263
JogiYo <jo930921@gmail.com>
2022-02-22 16:16:39 +0900

    BOJ 10926 : ??! Only IO

--- src/baek10000/BOJ_10926.java
+++ src/baek10000/BOJ_10926.java
@@ -1,4 +1,18 @@
 package baek10000;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// BOJ 10926 : ??!
+// Only IO
 public class BOJ_10926 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+        sb.append(input + "??!");
+
+        System.out.print(sb);
+    }
 }
