18632f7c47b08e87840e4a6c1d372dab8f4b568c
JogiYo <jo930921@gmail.com>
2022-05-09 16:20:03 +0900

    BOJ 25083 : Sprout input and output

---
+++ src/baek25000/BOJ_25083.java
@@ -0,0 +1,17 @@
+package baek25000;
+
+// BOJ 25083 : Sprout
+// input and output
+public class BOJ_25083 {
+    public static void main(String[] args) throws Exception{
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("         ,r'\"7\n");
+        sb.append("r`-_   ,'  ,/\n");
+        sb.append(" \\. \". L_r'\n");
+        sb.append("   `~\\/\n");
+        sb.append("      |\n");
+        sb.append("      |");
+        System.out.print(sb);
+    }
+}
