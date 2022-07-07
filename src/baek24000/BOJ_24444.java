c948534c1c8215a8541dfcfbbf8a9a142761a077
JogiYo <jo930921@gmail.com>
2022-07-07 16:48:09 +0900

    BOJ 24479 : Breadth First Search 1 use BFS, adjacency list

--- src/baek24000/BOJ_24444.java
+++ src/baek24000/BOJ_24444.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+// BOJ 24479 : Breadth First Search 1
+// use BFS, adjacency list
 public class BOJ_24444 {
     static BufferedReader br;
     static StringBuilder sb;
