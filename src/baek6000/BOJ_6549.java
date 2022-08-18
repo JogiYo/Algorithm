d4c72470102b397c53000ed3fd3c5e1428107d42
JogiYo <jo930921@gmail.com>
2022-08-18 16:40:40 +0900

    BOJ 6549 : largest rectangle in histogram(divide and conquer ver.) use divide and conquer

--- src/baek6000/BOJ_6549.java
+++ src/baek6000/BOJ_6549.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// BOJ 6549 : largest rectangle in histogram
+// use divide and conquer
 public class BOJ_6549 {
     static BufferedReader br;
     static StringBuilder sb;
@@ -26,10 +28,72 @@ public class BOJ_6549 {
                 histogram[i] = Integer.parseInt(st.nextToken());
             }
 
-            // solution
+            sb.append(getArea(0, n-1)).append("\n");
 
             histogram = null;
         }
         System.out.print(sb);
+    }
+
+    private static long getArea(int left, int right) {
+        /*
+         *  If the width is 1,
+         *  it is returned immediately because the height is the width.
+         */
+        if(left == right) return histogram[left];
+
+        int mid = (left + right) / 2;
+
+        long leftArea = getArea(left, mid);
+        long rightArea = getArea(mid+1, right);
+
+        long max = Math.max(leftArea, rightArea);
+
+        max = Math.max(max, getMidArea(left, right, mid));
+
+        return max;
+    }
+
+    // Method to find the area of ​​the midpoint area
+    private static long getMidArea(int left, int right, int mid) {
+        int left_dir = mid;
+        int right_dir = mid;
+
+        long height = histogram[mid];
+
+        long maxArea = height;
+
+        // Repeat until both ends are out of bounds
+        while(left < left_dir && right_dir < right) {
+            /*
+             * Select the higher value among the heights of the next two columns,
+             * but the updated height must be less than or equal to the existing height.
+             */
+            if(histogram[left_dir-1] < histogram[right_dir+1]) {
+                right_dir++;
+                height = Math.min(height, histogram[right_dir]);
+            }
+            else {
+                left_dir--;
+                height = Math.min(height, histogram[left_dir]);
+            }
+
+            maxArea = Math.max(maxArea, height * (right_dir - left_dir + 1));
+        }
+
+        // If the right section has not been searched until the end, it will search even.
+        while(right_dir < right) {
+            right_dir++;
+            height = Math.min(height, histogram[right_dir]);
+            maxArea = Math.max(maxArea, height * (right_dir - left_dir + 1));
+        }
+        // If the left section has not been searched until the end, it is searched even.
+        while(left < left_dir) {
+            left_dir--;
+            height = Math.min(height, histogram[left_dir]);
+            maxArea = Math.max(maxArea, height * (right_dir - left_dir + 1));
+        }
+
+        return maxArea;
     }
 }
