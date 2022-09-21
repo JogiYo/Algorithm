051eac92262f348219e3bd79a11b734db73b2100
JogiYo <jo930921@gmail.com>
2022-09-21 16:32:21 +0900

    BOJ 2805 -ing

---
+++ src/baek2000/BOJ_2805.java
@@ -0,0 +1,23 @@
+package baek2000;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2805 {
+    static BufferedReader br;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        StringTokenizer st= new StringTokenizer(br.readLine());
+
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        solution(k, n);
+        System.out.print(sb);
+    }
+}
