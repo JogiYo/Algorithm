20353f2c9a97ad5756da1a423d42945692a2b278
JogiYo <jo930921@gmail.com>
2022-10-12 17:16:20 +0900

    BOJ 1300 : Kth number binary search

--- src/baek1000/BOJ_1300.java
+++ src/baek1000/BOJ_1300.java
@@ -2,14 +2,12 @@ package baek1000;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-// ref : https://st-lab.tistory.com/281
+// BOJ 1300 : Kth number
 // binary search
 public class BOJ_1300 {
     static BufferedReader br;
     static StringBuilder sb;
-    static StringTokenizer st;
     public static void main(String[] args) throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
@@ -26,7 +24,27 @@ public class BOJ_1300 {
         long right = k;
 
         while(left < right) {
-            
+            long mid = (left + right) / 2; // Set any x(mid) to the median value
+            long count = 0;
+
+            /*
+             * The number of elements less than or equal to x by dividing the i-th row for any x
+             * Find the cumulative sum.
+             * In this case, the number of elements in each row must be summed in a line that does not exceed N (the number of columns).
+             */
+            for(int i = 1; i <= n; ++i) {
+                count += Math.min(mid / i, n);
+            }
+
+            // More count means that any number less than x(mid) is greater than B[K]
+            if(k <= count) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
         }
+
+        sb.append(left);
     }
 }
